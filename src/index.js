22dc60882e535f38c6295a5a79f25b4ba234d34d
AVZapevalov <106755729+AVZapevalov@users.noreply.github.com>
2023-10-20 15:57:47 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log('Great!');
+
+import './css/style.css';
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'main.html'
+        }),
+        new MiniCSSExtractPlugin()
+    ]
+};
